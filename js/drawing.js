69ecafc8e97ee70e5700f3a27f8b59011928a451
WearponiX <timofey_volk@mail.ru>
2019-06-27 02:30:51 +0300

    добавил поддержку тач-событий (сенсорные экраны)

--- js/drawing.js
+++ js/drawing.js
@@ -6,27 +6,64 @@ var canvasY = canvas.getBoundingClientRect().top + pageYOffset;
 
 canvas.addEventListener('mousedown', start);
 
-function start() {
+function initDrawing() {
   ctx.strokeStyle = getColor();
   ctx.lineWidth = sliderPin.value;
   ctx.lineJoin = 'round';
   ctx.lineCap = 'round';
   ctx.imageSmoothingQuality = 'high'
+};
+
+function start(event) {
+  event.preventDefault();
+  initDrawing();
   ctx.beginPath();
   ctx.moveTo((event.pageX - canvasX), (event.pageY - canvasY));
   event.target.addEventListener('mousemove', move);
 };
 
-function move() {
+function move(event) {
+  event.preventDefault();
   document.addEventListener('mouseup', end);
   ctx.lineTo((event.pageX - canvasX), (event.pageY - canvasY));
   ctx.stroke();
 };
 
-function end() {
+function end(event) {
+  event.preventDefault();
   event.target.removeEventListener('mousemove', move);
   document.removeEventListener('mouseup', end);
   ctx.moveTo((event.pageX - canvasX), (event.pageY - canvasY));
+  ctx.closePath();
+  ctx.stroke();
+};
+
+canvas.addEventListener('touchstart', startMobile);
+
+function startMobile(event) {
+  event.preventDefault();
+  initDrawing();
+  ctx.beginPath();
+  ctx.moveTo((event.changedTouches[0].pageX - canvasX), (event.changedTouches[0].pageY - canvasY));
+  event.target.addEventListener('touchmove', moveMobile);
+  console.log('start');
+
+};
+
+function moveMobile(event) {
+  event.preventDefault();
+  document.addEventListener('touchend', endMobile);
+  ctx.lineTo((event.changedTouches[0].pageX - canvasX), (event.changedTouches[0].pageY - canvasY));
+  ctx.stroke();
+};
+
+function endMobile(event) {
+  event.preventDefault();
+
+  console.log('end');
+  event.target.removeEventListener('touchmove', moveMobile);
+  document.removeEventListener('touchend', endMobile);
+  ctx.moveTo((event.changedTouches[0].pageX - canvasX), (event.changedTouches[0].pageY - canvasY));
   ctx.closePath();
   ctx.stroke();
 };
